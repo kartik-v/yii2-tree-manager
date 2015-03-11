3e53864513c950379e08303366d5a97da74f0088
Kartik Visweswaran <kartikv2@gmail.com>
2015-03-11 21:09:08 +0530

    Updates to release v1.0.0

--- models/Tree.php
+++ models/Tree.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2015
+ * @package yii2-tree-manager
+ * @version 1.0.0
+ */
+ 
 namespace kartik\tree\models;
 
 use Yii;
