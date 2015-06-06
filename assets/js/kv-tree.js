bd4643f00eaf91bd75b483ddbdf8a29acfe4c3c2
Chris Stebe <cstebe@iserv4u.com>
2015-06-07 01:57:02 +0200

    minor cleanup

--- assets/js/kv-tree.js
+++ assets/js/kv-tree.js
@@ -815,9 +815,8 @@
                 addCss($node, 'kv-selected');
                 self.check($node);
             }
-        },
-    }
-    ;
+        }
+    };
 
     $.fn.treeview = function (option) {
         var args = Array.apply(null, arguments), $this, data, options;
